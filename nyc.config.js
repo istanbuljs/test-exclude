60c18f6598169e49e4c3e1f52033da9880ec5c0c
Corey Farrell <git@cfware.com>
2019-09-24 13:59:53 -0400

    chore: Switch testing to tap (#31)

--- nyc.config.js
+++ nyc.config.js
@@ -6,11 +6,6 @@ const isWindows = process.platform === 'win32';
 
 module.exports = {
     all: true,
-    checkCoverage: true,
-    lines: 100,
-    statements: 100,
-    functions: 100,
-    branches: 100,
     exclude: [
         ...defaultExclude,
         'is-outside-dir.js',
---
+++ test/glob.js
@@ -0,0 +1,97 @@
+'use strict';
+const path = require('path');
+const t = require('tap');
+
+const exclude = require('../');
+
+async function testHelper(t, { options, args = [], label }) {
+    t.matchSnapshot(
+        exclude(options)
+            .globSync(...args)
+            .sort(),
+        label
+    );
+}
+
+const cwd = path.resolve(__dirname, 'fixtures/glob');
+const extension = '.js';
+const options = { cwd, extension };
+
+t.test('should exclude the node_modules folder by default', async t =>
+    Promise.all(
+        [
+            { options, label: 'js files' },
+            {
+                options: { cwd, extension: ['.json'] },
+                label: 'json files'
+            },
+            {
+                options: { cwd, extension: [] },
+                label: 'no extension'
+            },
+            {
+                options: { cwd, extension: ['.js', '.json'] },
+                label: 'js and json files'
+            },
+            {
+                options: { cwd: path.join(process.cwd(), 'test') },
+                args: [cwd],
+                label: 'absolute constructor cwd'
+            }
+        ].map(opts => testHelper(t, opts))
+    )
+);
+
+t.test('applies exclude rule ahead of include rule', t =>
+    testHelper(t, {
+        options: {
+            cwd,
+            extension,
+            include: ['file1.js', 'file2.js'],
+            exclude: ['file1.js']
+        }
+    })
+);
+
+t.test(
+    'allows node_modules folder to be included, if !node_modules is explicitly provided',
+    t =>
+        testHelper(t, {
+            options: {
+                cwd,
+                extension,
+                exclude: ['!node_modules']
+            }
+        })
+);
+
+t.test(
+    'allows specific node_modules folder to be included, if !node_modules is explicitly provided',
+    t =>
+        testHelper(t, {
+            options: {
+                cwd,
+                extension,
+                exclude: ['!node_modules/something/other.js']
+            }
+        })
+);
+
+t.test('allows negated exclude patterns', t =>
+    testHelper(t, {
+        options: {
+            cwd,
+            extension,
+            exclude: ['*.js', '!file1.js']
+        }
+    })
+);
+
+t.test('allows negated include patterns', t =>
+    testHelper(t, {
+        options: {
+            cwd,
+            include: ['*.js', '!file2.js']
+        }
+    })
+);
