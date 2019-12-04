100331b8bd09f6ea8e15645b7e903c4495f58843
Corey Farrell <git@cfware.com>
2019-12-04 10:19:52 -0500

    chore: Fix testing to deal with changes to tap/nyc integration (#37)
    This just disables tap/nyc integration and uses nyc directly as the
    integration cannot support conditional excludes.

--- nyc.config.js
+++ nyc.config.js
@@ -6,6 +6,11 @@ const isWindows = process.platform === 'win32';
 
 module.exports = {
     all: true,
+    checkCoverage: true,
+    lines: 100,
+    functions: 100,
+    branches: 100,
+    statements: 100,
     exclude: [
         ...defaultExclude,
         'is-outside-dir.js',
